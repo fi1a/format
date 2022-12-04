e35ce39bb13bf5fd1ab0626ac6d90935eb633768
Nikolay Gorohovsky <fi1a@icloud.com>
2022-12-04 07:33:29 +0500

    Обновлена зависимость fi1a/collection до версии 2.0

--- src/AST/INodes.php
+++ src/AST/INodes.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Fi1a\Format\AST;
 
-use Fi1a\Collection\DataType\IArrayObject;
+use Fi1a\Collection\DataType\ArrayObjectInterface;
 
 /**
  * Список
  */
-interface INodes extends IArrayObject
+interface INodes extends ArrayObjectInterface
 {
 }
