6407a159d4cb722620edd5d71736a9562e5ad289
Nikolay Gorohovsky <fi1a@icloud.com>
2022-12-31 11:37:22 +0500

    Функция спецификатор форматирования числа

---
+++ src/Specifier/Number.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fi1a\Format\Specifier;
+
+use InvalidArgumentException;
+
+/**
+ * Форматирование числа
+ */
+class Number implements SpecifierInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function format($value, ...$modifiers): string
+    {
+        if (count($modifiers) > 4) {
+            throw new InvalidArgumentException('Передано больше чем 4 модификатора');
+        }
+
+        $value = (float) $value;
+
+        $decimals = isset($modifiers[0]) ? (int) $modifiers[0] : 2;
+        $decimalSeparator = isset($modifiers[1]) && $modifiers[1] ? (string) $modifiers[1] : '.';
+        $thousandsSeparator = isset($modifiers[2]) && $modifiers[2] ? (string) $modifiers[2] : '';
+        $allowZeroDecimal = isset($modifiers[3]) && $modifiers[3] === true;
+
+        if (!$allowZeroDecimal && $decimals) {
+            $abs = abs($value - floor($value));
+            $comma = number_format($abs, $decimals, '.', '');
+            for ($index = 1; $index <= $decimals; $index++) {
+                if (mb_substr($comma, ($index * -1), 1) !== '0') {
+                    break;
+                }
+            }
+
+            return number_format(
+                $value,
+                ($decimals - $index + 1),
+                $decimalSeparator,
+                $thousandsSeparator
+            );
+        }
+
+        return number_format(
+            $value,
+            $decimals,
+            $decimalSeparator,
+            $thousandsSeparator
+        );
+    }
+}
--- src/Specifier/Price.php
+++ src/Specifier/Price.php
@@ -9,46 +9,34 @@ use InvalidArgumentException;
 /**
  * Форматирование цены
  */
-class Price implements SpecifierInterface
+class Price extends Number
 {
     /**
      * @inheritDoc
      */
     public function format($value, ...$modifiers): string
     {
-        if (count($modifiers) > 4) {
-            throw new InvalidArgumentException('Передано больше чем 4 модификатора');
+        if (count($modifiers) > 7) {
+            throw new InvalidArgumentException('Передано больше чем 7 модификатора');
         }
 
         $value = (float) $value;
 
-        $decimals = isset($modifiers[0]) && $modifiers[0] ? (int) $modifiers[0] : 2;
+        $decimals = isset($modifiers[0]) ? (int) $modifiers[0] : 2;
         $decimalSeparator = isset($modifiers[1]) && $modifiers[1] ? (string) $modifiers[1] : '.';
         $thousandsSeparator = isset($modifiers[2]) && $modifiers[2] ? (string) $modifiers[2] : '';
         $allowZeroDecimal = isset($modifiers[3]) && $modifiers[3] === true;
+        $round = isset($modifiers[4]) ? (int) $modifiers[4] : null;
+        $roundPrecision = isset($modifiers[5]) ? (int) $modifiers[5] : 0;
+        $floor = isset($modifiers[6]) && $modifiers[6] === true;
 
-        if (!$allowZeroDecimal && $decimals) {
-            $abs = abs($value - floor($value));
-            $comma = number_format($abs, $decimals, '.', '');
-            for ($index = 1; $index <= $decimals; $index++) {
-                if (mb_substr($comma, ($index * -1), 1) !== '0') {
-                    break;
-                }
-            }
-
-            return number_format(
-                $value,
-                ($decimals - $index + 1),
-                $decimalSeparator,
-                $thousandsSeparator
-            );
+        if ($round > 0) {
+            $value = round($value, $roundPrecision, $round);
+        }
+        if ($floor) {
+            $value = floor($value);
         }
 
-        return number_format(
-            $value,
-            $decimals,
-            $decimalSeparator,
-            $thousandsSeparator
-        );
+        return parent::format($value, $decimals, $decimalSeparator, $thousandsSeparator, $allowZeroDecimal);
     }
 }
